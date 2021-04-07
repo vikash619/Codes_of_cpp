92aeaf4c2f7763460f1c1d16db98a62dad4d04c9
Vikash Mishra <70462733+vikash619@users.noreply.github.com>
2021-04-07 18:46:09 +0530

    Create things in tree.cpp

---
+++ tree/things in tree.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+  int data;
+  struct node *left;
+  struct node *right;
+};
+
+int main()
+{
+
+  // Depth = No. of edges from root to node
+  // Height = No. of edges from node to depest leaf
+  // Siblings = Nodes belong to same parent
+  // Leaf = Node that have no child
+  // Binary Tree = Tree with max two children for each node or no node
+  return 0;
+}
