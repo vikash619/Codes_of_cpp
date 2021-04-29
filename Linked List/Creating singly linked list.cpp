f36c5c3deb23bce9a3173b439dbfff40761d56a5
Vikash Mishra <70462733+vikash619@users.noreply.github.com>
2021-04-29 12:39:29 +0530

    Update Creating singly linked list.cpp

--- Linked List/Creating singly linked list.cpp
+++ Linked List/Creating singly linked list.cpp
@@ -35,13 +35,41 @@ struct node* insertAtBeg(struct node* head, int data)
     struct node* temp = (struct node*)malloc(sizeof(struct node));
     temp->data = data;
     temp->next = NULL;
-    
-    struct node* ptr = head;
+
     temp->next = head;
     head = temp;
     return head;
 }
 
+struct node* deletetAtBeg(struct node* head)
+{
+    struct node* ptr = head;
+    head = ptr->next;
+    free(ptr);
+    return head;
+}
+
+
+struct node* insertAtmiddle(struct node* head, int data, int index)
+{
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->next = NULL;
+    
+    struct node* ptr = head;
+    int incre = 0;
+    
+    
+    while(incre < index-1 )
+    {
+        ptr = ptr->next;
+        incre++;
+    }
+    
+    temp->next = ptr->next;
+    ptr->next = temp;
+    return head;
+}
 
 void display(struct node* head)
 {
@@ -49,7 +77,7 @@ void display(struct node* head)
     
     while(ptr != NULL)
     {
-        cout<<ptr->data;
+        cout<<ptr->data<<" ";
         ptr = ptr->next;
     }
 }
@@ -62,8 +90,15 @@ int main()
     head = create(head, 30);
     head = create(head, 40);
     head = create(head, 50);
+    head = create(head, 60);
+    head = create(head, 70);
     head = insertAtBeg(head, 5);
     head = insertAtBeg(head, 2);
+    head = insertAtBeg(head, 1);
+    head = insertAtmiddle( head, 45, 5);
+    head = deletetAtBeg(head);
+    head = deletetAtBeg(head);
+    
     display(head);
 
     return 0;
