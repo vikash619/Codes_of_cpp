693f3bb5bfd1c8e8d16fb9d27f1c3e6f040aa165
Vikash Mishra <70462733+vikash619@users.noreply.github.com>
2021-05-02 17:39:11 +0530

    Create Counting leaf node in Tree or BST.cpp

---
+++ tree/Counting leaf node in Tree or BST.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+
+// Insert in BST
+struct node* insert(struct node* root, int data)
+{
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    if(root == NULL)
+    {
+        root = temp;
+        return root;
+    }
+    if(temp->data < root->data)
+    {
+        root->left = insert(root->left, data);
+    }
+    if(temp->data > root->data)
+    {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+int main()
+{
+    struct node* root = NULL;
+    
+    root = insert(root, 40);
+    root = insert(root, 30);
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    cout<<endl;
+    int Leaf = numberOfLeaves(root);
+    cout<<Leaf;
+    return 0;
+}
