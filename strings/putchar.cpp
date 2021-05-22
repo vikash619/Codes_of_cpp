bf733e478e841f6a67a347a790f6342d83738feb
Vikash Mishra <70462733+vikash619@users.noreply.github.com>
2021-05-22 13:42:49 +0530

    Create putchar.cpp

---
+++ strings/putchar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{   
+    int ch;
+    
+    for(ch = 'A'; ch <= 'Z'; ch++)
+    putchar(ch);                    // passing 'ch = A' means  we passing "65" integer 
+                                    // and it prints 'A' coz 65 = A
+                                    
+                                    //this function accepts integer argument and return 
+                                    //character representing that integer
+    
+    
+    return 0;
+}
