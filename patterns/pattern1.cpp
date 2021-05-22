d1e2f0a438c5ea69ffd68c7f9c79528c3bc01619
Vikash Mishra <70462733+vikash619@users.noreply.github.com>
2021-05-22 19:35:21 +0530

    Create pattern1.cpp

---
+++ patterns/pattern1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include <string.h>
+
+using namespace std;
+
+int main()
+{  
+    int row;
+    cin>>row;
+    
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < row; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+//*****
+//*****
+//*****
+//*****
+//*****
