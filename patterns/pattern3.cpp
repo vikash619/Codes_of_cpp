8daf238803d3e5d1e743d1f04efd6b13db953f82
Vikash Mishra <70462733+vikash619@users.noreply.github.com>
2021-05-22 19:39:34 +0530

    Create pattern3.cpp

---
+++ patterns/pattern3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include <string.h>
+
+using namespace std;
+
+int main()
+{  
+    int row;
+    cin>>row;
+    
+    for(int i = 1; i <=row; i++)
+    {
+        for(int j = 1; j <=row; j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
+
+//12345
+//12345
+//12345
+//12345
+//12345
